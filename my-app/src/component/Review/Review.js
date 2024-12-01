de7a2eb40b141cde7249efe12a77b6d1e5b19b7b
dhanubor <nikhilmistry121203@gmail.com>
2024-12-01 18:54:50 +0600

    y

--- my-app/src/component/Review/Review.js
+++ my-app/src/component/Review/Review.js
@@ -1,16 +1,18 @@
-import React from 'react';
+import React, { useContext, createContext } from 'react';
 
-const Review = () => {
-    return (
-        <div>
-                <h2>Review</h2>
-                <p>This is a sample review.</p>
-                <p>Author: John Doe</p>
-                <p>Rating: 4/5</p>
-                <p>Date: 2022-10-15</p>
-        
-        </div>
-    );
-};
+const UserContext = createContext();
+
+function DisplayUser() {
+  const user = useContext(UserContext);  // Context থেকে ডেটা অ্যাক্সেস
+  return <h1>Welcome, {user}!</h1>;
+}
+
+function Review() {
+  return (
+    <UserContext.Provider value="John Doe">
+      <DisplayUser />
+    </UserContext.Provider>
+  );
+}
 
 export default Review;
